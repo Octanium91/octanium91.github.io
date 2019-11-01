23ffa6b394609315be2f91206894039754e02347
octanium91 <octanium91@gmail.com>
2019-11-01 19:36:48 +0200

    test 2

---
+++ random-fill-react/build — копия/precache-manifest.a6b6b534aa6dd53b8c5ae90958443ace.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "472342bc8628c684462d6806b940e31e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3a7dd038fb1d43e7fcf2",
+    "url": "./static/css/main.20be73ac.chunk.css"
+  },
+  {
+    "revision": "a90844477a6a214079da",
+    "url": "./static/js/2.65a7bb89.chunk.js"
+  },
+  {
+    "revision": "3a7dd038fb1d43e7fcf2",
+    "url": "./static/js/main.70072e75.chunk.js"
+  },
+  {
+    "revision": "d308208d7a5fff0d2668",
+    "url": "./static/js/runtime-main.94124f04.js"
+  }
+]);
