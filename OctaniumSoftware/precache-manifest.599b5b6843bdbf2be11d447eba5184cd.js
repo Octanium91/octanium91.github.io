d126ac49d926f532ec2e78609485f4fd648fe4bd
octanium91 <octanium91@gmail.com>
2019-12-19 22:56:51 +0200

    update website, jump on react

---
+++ OctaniumSoftware/precache-manifest.599b5b6843bdbf2be11d447eba5184cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce7614b19381f22fc40f85a783467e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2f0198ba73a89f027a8a",
+    "url": "./static/css/main.d5e7991a.chunk.css"
+  },
+  {
+    "revision": "e829ba0c08aada1a9fac",
+    "url": "./static/js/2.c9d55833.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.c9d55833.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f0198ba73a89f027a8a",
+    "url": "./static/js/main.ffe538fa.chunk.js"
+  },
+  {
+    "revision": "e079beeb96f1928bf86f",
+    "url": "./static/js/runtime-main.c68a803c.js"
+  },
+  {
+    "revision": "08ce04714c7bb9eb47769c4f8a502dba",
+    "url": "./static/media/ASC_ICON.08ce0471.webp"
+  },
+  {
+    "revision": "b15e25c9daf450d415fbd0376820ac5e",
+    "url": "./static/media/ERI_icon.b15e25c9.webp"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/Reactlogo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21159068ba377d9d7d2fa42a3f0db8af",
+    "url": "./static/media/my_photo.21159068.jpg"
+  },
+  {
+    "revision": "ee053e122427b5ee6c55a1e215934f15",
+    "url": "./static/media/xda-flat-circle-icon-256.ee053e12.webp"
+  }
+]);
