4ac5fbebddb342f9d345f28d79ac52a50f63a0f8
octanium91 <octanium91@gmail.com>
2020-05-11 00:17:24 +0300

    optimization-video

---
+++ p/optimization-video/react-demo-slow/precache-manifest.9da697b85a92b14a3eb18ce9ff773c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea88565d3fe3bcbfa3c5a092817579b4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9dfcbca2cc8c1296b0f8",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42e6e0ef08bd6af54f2e",
+    "url": "./static/js/2.03b3c29a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.03b3c29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfcbca2cc8c1296b0f8",
+    "url": "./static/js/main.a442268a.chunk.js"
+  },
+  {
+    "revision": "b3de56d524d8b61074c1",
+    "url": "./static/js/runtime-main.59214c72.js"
+  }
+]);
---
+++ p/optimization-video/react-demo/precache-manifest.d0d8ed3d13e10555dd3fc6e5712e37c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb95f2c9f8595b0a89cb13445f9d287b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c2511d756fcd141778e",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42e6e0ef08bd6af54f2e",
+    "url": "./static/js/2.03b3c29a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.03b3c29a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c2511d756fcd141778e",
+    "url": "./static/js/main.abbb4dea.chunk.js"
+  },
+  {
+    "revision": "b3de56d524d8b61074c1",
+    "url": "./static/js/runtime-main.59214c72.js"
+  }
+]);
