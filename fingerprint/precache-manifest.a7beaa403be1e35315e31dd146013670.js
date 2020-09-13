7f02231afa2cf79a35bc62f4ab3a1d51e0a65033
octanium91 <octanium91@gmail.com>
2020-09-13 20:19:06 +0300

    publish fingerprint project

---
+++ fingerprint/precache-manifest.a7beaa403be1e35315e31dd146013670.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45685500838fd85f0a77b613b4a78bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c696ecb276dce6250b0",
+    "url": "./static/css/main.ac117957.chunk.css"
+  },
+  {
+    "revision": "92a643053c7c0cb3a93f",
+    "url": "./static/js/2.943180cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.943180cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c696ecb276dce6250b0",
+    "url": "./static/js/main.b8dd3eea.chunk.js"
+  },
+  {
+    "revision": "0c7822e7b1cb1de5c16f",
+    "url": "./static/js/runtime-main.ce5096ff.js"
+  },
+  {
+    "revision": "db801db6bb720e83b086891684c6cc94",
+    "url": "./static/media/13.db801db6.png"
+  },
+  {
+    "revision": "8072147180ec3510566dbdf273da7071",
+    "url": "./static/media/14.80721471.png"
+  },
+  {
+    "revision": "61b4e7260ceed07bb456a7c69ef5a0b3",
+    "url": "./static/media/15.61b4e726.png"
+  },
+  {
+    "revision": "08c4668e48bd21a3d0aa6a44d854b6d3",
+    "url": "./static/media/16.08c4668e.png"
+  },
+  {
+    "revision": "3bb51a5829fde408756a41b9f60d27fd",
+    "url": "./static/media/17.3bb51a58.png"
+  },
+  {
+    "revision": "1282dfbbbe84b0854a761e0cd9e2802b",
+    "url": "./static/media/18.1282dfbb.png"
+  },
+  {
+    "revision": "88b04e775f54103da645da5c322fb6b7",
+    "url": "./static/media/19.88b04e77.png"
+  },
+  {
+    "revision": "ba35d59ecdb6777cfbf7e584c776df98",
+    "url": "./static/media/4.ba35d59e.png"
+  },
+  {
+    "revision": "3c7c6697cd7cd2e76a095a954d81ffee",
+    "url": "./static/media/8.3c7c6697.png"
+  },
+  {
+    "revision": "a17a7c49d035540afcc0243f3760a527",
+    "url": "./static/media/9.a17a7c49.png"
+  },
+  {
+    "revision": "e51fb0b6e036d06bd3f5c5a70161608f",
+    "url": "./static/media/logofp.e51fb0b6.png"
+  }
+]);
